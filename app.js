d68bc21c8f5452ce0d8c2740ad51d52fac05d025
Andy Chuong <andy.chuong@colorado.edu>
2018-10-17 15:56:13 -0600

    cors

--- app.js
+++ app.js
@@ -25,7 +25,8 @@ app.options('/', (req,res,next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next()
+  res.send(200)
+  
 })
 
 app.use(logger('dev'));
