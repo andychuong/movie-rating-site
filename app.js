9327962d9f767346bf22eef183b55627e516c0e2
Andy Chuong <andy.chuong@colorado.edu>
2018-10-17 15:56:54 -0600

    cors?

--- app.js
+++ app.js
@@ -23,10 +23,10 @@ app.use(function(req, res, next) {
 
 app.options('/', (req,res,next) => {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
+  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   res.send(200)
-  
+
 })
 
 app.use(logger('dev'));
